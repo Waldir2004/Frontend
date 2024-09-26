196fa0799199822ac4f57dbbb9ab1a0b548e5b88
waldir2004 <waldiraltamar2004@gmail.com>
2024-09-26 16:24:50 -0500

    module rol

--- components/create-rol-component/create-rol-component.js
+++ components/create-rol-component/create-rol-component.js
@@ -14,7 +14,7 @@ class create_rol_component extends HTMLElement {
             .then(html => {
                 this.innerHTML = html;
                 this.attachEventListeners();
-                this.populateStates('state');
+                this.populateModules();
             })
             .catch(error => {
                 console.error("Error al cargar el archivo HTML:", error);
@@ -38,38 +38,50 @@ class create_rol_component extends HTMLElement {
             });
     }
 
-    populateStates(selectId, selectedState) {
-        axios.get('http://127.0.0.1:8000/get_parameter_values/2')
-            .then(function(response) {
-                let data = response.data.resultado;
-                let selectState = document.getElementById(selectId);
-                selectState.innerHTML = "";
-
-                data.forEach(param => {
-                    let option = document.createElement("option");
-                    option.value = param.id;
-                    option.text = param.name;
-                    if (param.id == selectedState) {
-                        option.selected = true;
-                    }
-                    selectState.appendChild(option);
+    populateModules() {
+        axios.get('http://127.0.0.1:8000/get_modules')  // Asume que tienes una API que devuelve los módulos
+            .then(response => {
+                const modules = response.data.resultado;  // Acceder a la propiedad 'resultado'
+                const modulesList = document.getElementById('modules-list');
+    
+                // Limpiar cualquier contenido anterior en la lista de módulos
+                modulesList.innerHTML = '';
+    
+                modules.forEach(module => {
+                    const checkbox = document.createElement('input');
+                    checkbox.type = 'checkbox';
+                    checkbox.value = module.id;
+                    checkbox.id = `module-${module.id}`;
+                    checkbox.classList.add('form-check-input');
+    
+                    const label = document.createElement('label');
+                    label.htmlFor = `module-${module.id}`;
+                    label.classList.add('form-check-label');
+                    label.innerText = module.name;
+    
+                    const div = document.createElement('div');
+                    div.classList.add('form-check');
+                    div.appendChild(checkbox);
+                    div.appendChild(label);
+    
+                    modulesList.appendChild(div);
                 });
             })
-            .catch(function(error) {
-                console.error('Error fetching states: ', error);
+            .catch(error => {
+                console.error("Error al obtener los módulos:", error);
             });
     }
 
     attachEventListeners() {
-        const registerButton = this.querySelector("button[onclick='cargarLOGICA()']");
+        const registerButton = this.querySelector("#register-role-button");
         if (registerButton) {
             registerButton.addEventListener("click", () => this.cargarLOGICA());
         }
-
+    
         const registerRoleModal = this.querySelector("#registerRoleModal");
         if (registerRoleModal) {
             $(registerRoleModal).on('show.bs.modal', () => {
-                this.populateStates('state');
+                this.populateModules();
             });
         }
     }
